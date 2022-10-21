f7f0a60bf27f252479db8f4cd6551d913fd75f88
icebeal <darrenthiores@gmail.com>
2022-10-21 12:16:33 +0700

    + Added Icon in Top Bar + Added + Button under User Profile Screen in Feed

--- app/src/main/java/com/dev/tiktok_compose/presentation/component/Item.kt
+++ app/src/main/java/com/dev/tiktok_compose/presentation/component/Item.kt
@@ -1,9 +1,11 @@
 package com.dev.tiktok_compose.presentation.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.dev.tiktok_compose.data.model.Feed
@@ -67,14 +70,35 @@ private fun HomeSideBar(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Image(
+        Box(
             modifier = Modifier
                 .size(48.dp)
-                .clip(CircleShape)
-                .border(2.dp, MaterialTheme.colors.onBackground, CircleShape),
-            painter = rememberAsyncImagePainter(profile_picture),
-            contentDescription = "profile picture"
-        )
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(48.dp)
+                    .align(Alignment.Center)
+                    .clip(CircleShape)
+                    .border(2.dp, MaterialTheme.colors.onBackground, CircleShape),
+                painter = rememberAsyncImagePainter(profile_picture),
+                contentDescription = "profile picture"
+            )
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(20.dp)
+                    .align(Alignment.BottomCenter)
+                    .offset(
+                        y = 10.dp
+                    )
+                    .background(
+                        color = Color.Red,
+                        shape = CircleShape
+                    ),
+                tint = MaterialTheme.colors.onBackground
+            )
+        }
 
         IconWithText(
             modifier = Modifier
--- app/src/main/java/com/dev/tiktok_compose/presentation/component/TopBar.kt
+++ app/src/main/java/com/dev/tiktok_compose/presentation/component/TopBar.kt
@@ -1,14 +1,16 @@
 package com.dev.tiktok_compose.presentation.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LiveTv
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -26,6 +28,40 @@ fun HomeTopBar(
     pagerState: PagerState,
     allScreens: List<HomeTab>
 ) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.LiveTv,
+            contentDescription = null,
+            modifier = Modifier
+                .size(24.dp)
+        )
+        HomeTabRow(
+            modifier = Modifier
+                .weight(1f),
+            pagerState = pagerState,
+            allScreens = allScreens
+        )
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = null,
+            modifier = Modifier
+                .size(24.dp)
+        )
+    }
+}
+
+@ExperimentalPagerApi
+@Composable
+fun HomeTabRow(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState,
+    allScreens: List<HomeTab>
+) {
     val coroutineScope = rememberCoroutineScope()
 
     TabRow(
@@ -39,7 +75,7 @@ fun HomeTopBar(
             Box(
                 modifier = Modifier
                     .pagerTabIndicatorOffset(pagerState, tabPositions)
-                    .padding(horizontal = 28.dp)
+                    .padding(horizontal = 32.dp)
                     .height(4.dp)
                     .clip(RoundedCornerShape(8.dp))
                     .background(color = MaterialTheme.colors.onBackground)
